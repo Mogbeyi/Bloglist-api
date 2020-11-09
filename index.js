96fdac354d1c5cf859a9c8b09a1c46755667b031
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-11-09 23:54:45 +0100

    solve ex4.4

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const app = require('./app') 
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const http = require('http')
+const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const http = require("http");
 
-const server = http.createServer(app)
-const express = require('express')
-const Blog = require('./models/blog')
+const server = http.createServer(app);
+const express = require("express");
+const Blog = require("./models/blog");
 
-app.use(express.json())
+app.use(express.json());
 
- server.listen(config.PORT, () => {
-   logger.info(`Server running on port ${config.PORT}`)
- })
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
-    return 1;
-}
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.map((blog) => blog.likes).reduce((a, b) => a + b, 0);
+};
 
 module.exports = {
-    dummy
-}
+  dummy,
+  totalLikes
+};
