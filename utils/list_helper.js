13db3a856bf745190a1b5665755707c377ba8c97
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-11-10 00:31:30 +0100

    solve ex4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.map((blog) => blog.likes).reduce((a, b) => a + b, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes);
+  const maxLikes = Math.max(...likes);
+
+  return blogs.filter((blog) => blog.likes === maxLikes);
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 };
